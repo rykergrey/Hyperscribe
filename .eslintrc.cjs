54217f1825a088d1fc5be2ab64bf62fc5aa015cd
Ryker Grey <ryker@hyperscript.ai>
2024-09-13 15:15:33 -0700

    Updated ESLint and TypeScript configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,9 @@ module.exports = {
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'react-hooks/exhaustive-deps': 'warn'
-    }
+    },
+    parserOptions: {
+      project: './tsconfig.json'
+    },
+    ignorePatterns: ['jest.config.js', 'next.config.js'] // Add this line
   };
-  
