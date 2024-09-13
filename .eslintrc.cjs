97f6641c32c528d4feea4cf33a19854fec39b2af
Ryker Grey <ryker@hyperscript.ai>
2024-09-13 15:58:52 -0700

    Fix ESLint configuration for Vercel deployment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,17 @@
 module.exports = {
-    extends: [
-      'next/core-web-vitals',
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react-hooks/exhaustive-deps': 'warn'
-    },
-    parserOptions: {
-      project: './tsconfig.json'
-    },
-    ignorePatterns: ['jest.config.js', 'next.config.js'] // Add this line
-  };
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  ignorePatterns: ['jest.config.js', 'next.config.js']
+};
